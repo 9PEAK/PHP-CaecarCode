4c7f7bfed99ac9532f52a16adf1c44c17a9e6f87
Kane <52653784@qq.com>
2018-04-04 14:07:56 +0800

    算法更新

--- src/FlipCode.php
+++ src/FlipCode.php
@@ -12,11 +12,11 @@ class FlipCode extends Base {
 	static function encode ($str)
 	{
 
-		$max = strlen($str);
+		$max = strlen(self::$STRING);
 		for ($i=0; $i<$max; $i++) {
 			$n = strpos(self::$STRING, $str[$i]); // 字符当前位置
 			if ( $n===false )continue;
-			$n = $max-$n;
+			$n = $max-$n-1;
 			$str[$i] = self::offset(self::$STRING[$n], self::$OFFSET);
 		}
 		return $str;
@@ -26,11 +26,11 @@ class FlipCode extends Base {
 
 	static function decode ($str)
 	{
-		$max = strlen($str);
+		$max = strlen(self::$STRING);
 		for ($i=0; $i<$max; $i++) {
 			$n = strpos(self::$STRING, $str[$i]); // 字符当前位置
 			if ( $n===false )continue;
-			$n = $max-$n;
+			$n = $max-$n-1;
 			$str[$i] = self::offset(self::$STRING[$n], self::$OFFSET*-1);
 		}
 
